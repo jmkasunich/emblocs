b141295187307bf8b279a973a9eb673b70f7dfd4
John Kasunich <jmkasunich@fastmail.fm>
2025-02-17 23:41:32 -0500

    initial work on GPIO driver

--- src/tmp_gpio.c
+++ src/tmp_gpio.c
@@ -3,64 +3,7 @@
 
 #include "emblocs.h"
 #include "platform.h"
-
-/*********************************************
- * 
- * GPIO management:
- * 
- * For every pin on the MCU, must define how it is used
- *     GPIO Input
- *     GPIO Output
- *     Alternate Function (and which one)
- *     Analog
- * If it is an output (GPIO or Alternate function), must 
- * select speed and push-pull or open-drain
- * If it is GPIO, must decide if it will be mapped to
- * an emblocs pin (and driven by gpio_read or gpio_write
- * functions) or controlled directly
- * 
- * 
- */
-
-typedef enum {
-    GPOUT = 0,          // no blocs pins; GP output control by registers
-    ALTFUNCT = 1,       // no blocs pins; choose an alternate function
-    GPIN = 2,           // no blocs pins; GP input
-    ANALOG = 3,         // no blocs pins; analog
-    BL_IN = 4,          // exports in pin
-    BL_OUT = 5,         // exports out pin
-    BL_IO = 6,          // exports in, out, and output enable pins
-} gpio_pin_mode_t;
-
-#define GPIO_MODE_MASK (0x007)
-
-typedef enum {
-    PUSHPULL = 0,
-    OPENDRAIN = 0x08
-} gpio_output_type_t;
-
-#define GPIO_OUTPUT_TYPE_MASK (0x008)
-
-typedef enum {
-    SPD_SLOW = 0,
-    SPD_MED = 0x010,
-    SPD_FAST = 0x20,
-    SPD_VFAST = 0x30
-} gpio_output_speed_t;
-
-#define GPIO_OUTPUT_SPEED_MASK (0x030)
-
-typedef enum {
-    NO_PU_PD = 0,
-    PULL_UP = 0x40,
-    PULL_DOWN = 0x80
-} gpio_pu_pd_t;
-
-#define GPIO_PU_PD_MASK (0x0C0)
-
-#define GPIO_ALT_FUNCT_MASK (0xF00)
-#define GPIO_ALT_FUNCT_SHIFT (8)
-
+#include "tmp_gpio.h"
 
 
 // instance data structure - one copy per instance in RAM
@@ -71,25 +14,47 @@ typedef struct bl_gpio_inst_s {
 
 _Static_assert((sizeof(bl_gpio_inst_t) < 32767), "instance structure too large");
 
+// pin name strings
+// (pin definitions are created dynamically, but the strings must exist in flash)
+char const * const pin_names_in[16] = {
+    "00_in", "01_in", "02_in", "03_in",
+    "04_in", "05_in", "06_in", "07_in",
+    "08_in", "09_in", "10_in", "11_in",
+    "12_in", "13_in", "14_in", "15_in"
+};
+
+char const * const pin_names_out[16] = {
+    "00_out", "01_out", "02_out", "03_out",
+    "04_out", "05_out", "06_out", "07_out",
+    "08_out", "09_out", "10_out", "11_out",
+    "12_out", "13_out", "14_out", "15_out"
+};
+
+char const * const pin_names_io[16] = {
+    "00_io", "01_io", "02_io", "03_io",
+    "04_io", "05_io", "06_io", "07_io",
+    "08_io", "09_io", "10_io", "11_io",
+    "12_io", "13_io", "14_io", "15_io"
+};
+
+
 // array of pin definitions - one copy in FLASH
 static bl_pin_def_t const bl_gpio_pins[] = {
     { "time", BL_TYPE_U32, BL_DIR_OUT, offsetof(bl_gpio_inst_t, bits[0])}
 };
 
-static void bl_gpio_read_funct(void *ptr);
-static void bl_gpio_write_funct(void *ptr);
+static void bl_gpio_read_funct(void *ptr, uint32_t period_ns);
+static void bl_gpio_write_funct(void *ptr, uint32_t period_ns);
 
-/*
 // array of function definitions - one copy in FLASH
 static bl_funct_def_t const bl_gpio_functs[] = {
-    { "read", &bl_gpio_read_funct },
-    { "write", &bl_gpio_write_funct }
+    { "read", BL_NO_FP, &bl_gpio_read_funct },
+    { "write", BL_NO_FP, &bl_gpio_write_funct }
 };
-*/
+
 
 /* component-specific setup function */
-bl_inst_meta_t * gpio_setup(char const *inst_name, struct bl_comp_def_s *comp_def, void *personality);
-
+bl_inst_meta_t * gpio_setup(char const *inst_name, struct bl_comp_def_s const *comp_def, void const *personality);
 
 
 // component definition - one copy in FLASH
@@ -98,23 +63,33 @@ bl_comp_def_t const bl_gpio_def = {
     gpio_setup,
     sizeof(bl_gpio_inst_t),
     _countof(bl_gpio_pins),
-//    ARRAYCOUNT(bl_gpio_functs),
-    &(bl_gpio_pins[0])
-//    &(bl_gpio_functs[0])
+    _countof(bl_gpio_functs),
+    bl_gpio_pins,
+    bl_gpio_functs
 };
 
-// realtime code - one copy in FLASH
-static void bl_gpio_read_funct(void *ptr)
+
+/* component-specific setup function */
+bl_inst_meta_t * gpio_setup(char const *inst_name, struct bl_comp_def_s const *comp_def, void const *personality)
 {
+
+}
+
+
+// realtime code - one copy in FLASH
+static void bl_gpio_read_funct(void *ptr, uint32_t period_ns)
+{
+    (void)period_ns;
     bl_gpio_inst_t *p = (bl_gpio_inst_t *)ptr;
 
     p->tsc = tsc_read();
 }
 
-static void bl_gpio_write_funct(void *ptr)
+static void bl_gpio_write_funct(void *ptr, uint32_t period_ns)
 {
+    (void)period_ns;
     bl_gpio_inst_t *p = (bl_gpio_inst_t *)ptr;
 
-    *(p->bits) = tsc_read() - p->tsc;
+    p->tsc = tsc_read() - p->tsc;
 }
 
