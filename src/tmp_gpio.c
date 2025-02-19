0a0d2ce289c968e1a89a52699d338175df70e972
John Kasunich <John_Kasunich@steris.com>
2025-02-19 14:18:44 -0500

    preliminary realtime code for GPIO driver

--- src/tmp_gpio.c
+++ src/tmp_gpio.c
@@ -8,7 +8,12 @@
 
 // instance data structure - one copy per instance in RAM
 typedef struct bl_gpio_inst_s {
-    bl_pin_bit_t bits[16];
+    bl_pin_bit_t *output_pins;
+    bl_pin_bit_t *out_ena_pins;
+    uint16_t output_bitmask;
+    uint16_t out_ena_bitmask;
+    bl_pin_bit_t *input_pins;
+    uint16_t input_bitmask;
 	uint32_t tsc;
 } bl_gpio_inst_t;
 
@@ -40,7 +45,7 @@ char const * const pin_names_io[16] = {
 
 // array of pin definitions - one copy in FLASH
 static bl_pin_def_t const bl_gpio_pins[] = {
-    { "time", BL_TYPE_U32, BL_DIR_OUT, offsetof(bl_gpio_inst_t, bits[0])}
+    { "time", BL_TYPE_U32, BL_DIR_OUT, offsetof(bl_gpio_inst_t, tsc)}
 };
 
 static void bl_gpio_read_funct(void *ptr, uint32_t period_ns);
@@ -76,20 +81,62 @@ bl_inst_meta_t * gpio_setup(char const *inst_name, struct bl_comp_def_s const *c
 }
 
 
+static volatile uint16_t IDR;
+
 // realtime code - one copy in FLASH
 static void bl_gpio_read_funct(void *ptr, uint32_t period_ns)
 {
-    (void)period_ns;
+    (void)period_ns;  // not used
     bl_gpio_inst_t *p = (bl_gpio_inst_t *)ptr;
+    uint32_t shifter;
+    bl_pin_bit_t *pin;
 
-    p->tsc = tsc_read();
+    // read the hardware into low half of shifter
+    shifter = IDR;
+    // put bitmask in high half of shifter
+    shifter |= ((uint32_t)(p->input_bitmask) << 16);
+    pin = p->input_pins;
+    while ( shifter & 0xFFFF0000 ) {
+        if ( shifter & 0x00010000) {
+            // a blocs pin exists, set its value
+            **pin = shifter & 0x00000001;
+            pin++;
+        }
+        shifter >>= 1;
+    }
 }
+
+
+
+
+static volatile uint32_t BSRR;
 
 static void bl_gpio_write_funct(void *ptr, uint32_t period_ns)
 {
-    (void)period_ns;
+    (void)period_ns;  // not used
     bl_gpio_inst_t *p = (bl_gpio_inst_t *)ptr;
+    uint32_t bitmask, active_bit, bsrr;
+    bl_pin_bit_t *pin;
 
+    bitmask = p->output_bitmask;
+    active_bit = 1;
+    bsrr = 0;
+    pin = p->output_pins;
+    while ( bitmask != 0 ) {
+        if ( bitmask & active_bit ) {
+            // a blocs pin exists
+            if ( **(pin++) ) {    // test pin and update ptr for next one
+                // write to set half of BSRR
+                bsrr |= active_bit;
+            } else {
+                // write to reset half of BSRR
+                bsrr |= (active_bit << 16);
+            }
+            bitmask &= ~active_bit;
+        }
+        active_bit <<= 1;
+    }
+    BSRR = bsrr;
     p->tsc = tsc_read() - p->tsc;
 }
 
