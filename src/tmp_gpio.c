613066b29e225a2a332983b6c8fe1f06b07b21e4
John Kasunich <jmkasunich@fastmail.fm>
2025-02-19 21:30:57 -0500

    change how pin names are stored; saves almost 300 bytes

--- src/tmp_gpio.c
+++ src/tmp_gpio.c
@@ -25,21 +25,21 @@ _Static_assert((sizeof(bl_gpio_inst_t) < 32767), "instance structure too large")
 
 // pin name strings
 // (pin definitions are created dynamically, but the strings must exist in flash)
-char const * const pin_names_in[16] = {
+char const pin_names_in[16][6] = {
     "00_in", "01_in", "02_in", "03_in",
     "04_in", "05_in", "06_in", "07_in",
     "08_in", "09_in", "10_in", "11_in",
     "12_in", "13_in", "14_in", "15_in"
 };
 
-char const * const pin_names_out[16] = {
+char const pin_names_out[16][7] = {
     "00_out", "01_out", "02_out", "03_out",
     "04_out", "05_out", "06_out", "07_out",
     "08_out", "09_out", "10_out", "11_out",
     "12_out", "13_out", "14_out", "15_out"
 };
 
-char const * const pin_names_io[16] = {
+char const pin_names_io[16][6] = {
     "00_io", "01_io", "02_io", "03_io",
     "04_io", "05_io", "06_io", "07_io",
     "08_io", "09_io", "10_io", "11_io",
