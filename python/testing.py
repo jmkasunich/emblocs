9d1b8dd05149039c4e95cdb2f89932c8fafdda50
John Kasunich <jmkasunich@fastmail.fm>
2026-01-12 02:14:58 -0500

    improved type checking

--- python/testing.py
+++ python/testing.py
@@ -14,76 +14,89 @@ def read_json_file(filename):
         print(f"Error decoding '{filename}': {e}")
     return filedata
 
-def check_lists(provided, required, optional, message):
-    missing = set(required) - set(provided)
-    allowed = required + optional
-    unexpected = set(provided) - set(allowed)
-    seen = set()
-    duplicates = []
-    for item in provided:
-        if item in seen:
-            if item not in duplicates:
-                duplicates.append(item)
-        else:
-            seen.add(item)
-    if missing:
-        print(f"{message}: missing item(s): {missing}")
-    if unexpected:
-        print(f"{message}: unexpected item(s): {unexpected}")
-    if duplicates:
-        print(f"{message}: duplcate item(s): {duplicates}")
-    if missing or unexpected or duplicates:
-        return False
-    return True
 
-def validate_block(block_dict):
-    required_keys=["name", "type"]
-    optional_keys=[]
-    return check_lists(block_dict.keys(), required_keys, optional_keys, "block")
 
-def validate_signal(signal_dict):
-    required_keys=["name", "type", "links"]
-    optional_keys=["value"]
-    return check_lists(signal_dict.keys(), required_keys, optional_keys, "signal")
+template = ( dict, {
+             'blocks'  : ( list, ( dict, { 'name' : str, 'type' : str } ) ),
+             'signals' : ( list, ( dict, { 'name' : str, 'type' : ( str, [ 'bit', 's32', 'u32', 'float' ] ), 'value' : None, 'links' : ( list, str ) } ) ),
+             'threads' : ( list, ( dict, { 'name' : str, 'period' : int, 'runs' : ( list, str ) } ) ) } )
 
-def validate_thread(thread_dict):
-    required_keys=["name", "runs"]
-    optional_keys=["period"]
-    return check_lists(thread_dict.keys(), required_keys, optional_keys, "thread")
+def check_type(name, item, pattern):
+    '''
+    checks the type of a data item, which can be a complex nexted data structure
 
-def validate_structure(model):
-    if not isinstance(model, dict):
-        print(f"toplevel should be dict, not {type(model)}")
-        return False
-    validators = { "blocks" : validate_block, "signals" : validate_signal, "threads": validate_thread }
-    seen=set()
-    duplicates=[]
-    if not check_lists(model.keys(), list(validators.keys()), [], "toplevel"):
-        return False
-    for key, value in model.items():
-        if not isinstance(value, list):
-            print(f"'{key} should be list, not {type(value)}")
+    :param name: name of item; will be printed in error messages
+    :param item: item to be checked
+    :param pattern: pattern to check 'item' against, can be one of the following:
+        None: no type checking is applied
+        a single type: checks that 'item' is an instance of type
+        a tuple consisting of a type followed by type-specific data, as follows:
+            str followed by list of strings: 'item' must be a string in the list
+            list followed by a sub-pattern: 'item' must be a list, and the
+                sub-pattern is recursively applied to each list element
+            dict followed by a dict of key:sub-pattern pairs; 'item' must be
+                a dict containing all of and only the specified keys, and each
+                key's sub-pattern is recursively applied to that key's value
+    '''
+    if pattern == None:
+        return True
+    if not isinstance(pattern, tuple):
+        # simple pattern, does type match only
+        if not isinstance(item, pattern):
+            print(f"'{name}' is '{item}', expected {pattern}")
             return False
-        for item in value:
+    else:
+        # tuple pattern
+        type = pattern[0]
+        if type == str:
+            # check against list of allowed strings
+            allowed = pattern[1]
+            if not item in allowed:
+                print(f"'{name}' is '{item}', expected one of {allowed}")
+                return False
+        elif type == list:
+            if not isinstance(item, list):
+                print(f"'{name}' is '{item}', expected {list}")
+                return False
+            subpattern = pattern[1]
+            for index, subitem in enumerate(item):
+                if not check_type(f"{name}[{index}]", subitem, subpattern):
+                    return False
+        elif type == dict:
             if not isinstance(item, dict):
-                print(f"{key} list item should be dict, not {type(item)}")
+                print(f"'{name}' is '{item}', expected {dict}")
                 return False
-            if not validators[key](item):
+            patternkeys = set(pattern[1].keys())
+            itemkeys = set(item.keys())
+            missing = patternkeys - itemkeys
+            if missing:
+                print(f"'{name}' missing required keys {missing}")
                 return False
-            if item["name"] in seen:
-                if not item["name"] in duplicates:
-                    duplicates.append(item["name"])
-            else:
-                seen.add(item["name"])
-    if duplicates:
-        print(f"duplicated name(s): {duplicates}")
-        return False
-    print(f"{seen=}")
+            unexpected = itemkeys - patternkeys
+            if unexpected:
+                print(f"'{name}' contains unexpected keys {unexpected}")
+                return False
+            for key, subitem in item.items():
+                if not check_type(f"{key}", subitem, pattern[1][key]):
+                    print(f"  in {name}")
+                    return False
+        else:
+            print(f"unknown pattern {pattern}")
+            return False
     return True
 
 
+# template = ( dict, { "foo" : str, "bar" : ( str, [ "hello", "world" ]) } )
+# object = { 'bar': "hello", 'foo': "world!" }
 
+# print(f"{template=}")
+# print(f"{object=}")
+# print(f"{check_type("toplevel", object, template)=}")
+
+
+
+print(f"{template=}")
 fd = read_json_file("sample.json")
-#print(f"{fd=}")
-print(f"{validate_structure(fd)=}")
+print(f"{fd=}")
+print(f"{check_type('toplevel', fd, template)=}")
 
